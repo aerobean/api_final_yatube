df7c76fa817a5a7d6f6a85e1aa67a4a8fce1a513
Max Astashev <aerobean@gmail.com>
2022-11-11 12:21:32 +0300

    Sprint 9. Review. Final

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -51,14 +51,12 @@ class FollowViewSet(mixins.CreateModelMixin,
                     mixins.ListModelMixin,
                     viewsets.GenericViewSet):
     serializer_class = FollowSerializer
-    pagination_class = pagination.LimitOffsetPagination
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (SearchFilter,)
     search_fields = ('following__username', 'user__username',)
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.request.user.username)
-        return user.follower
+        return self.request.user.follower
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
